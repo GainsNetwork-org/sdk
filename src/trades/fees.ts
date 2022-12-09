cb467d42512da1cb3326ef7770a00dfe725a65e9
thebuidler <thebuidler@pm.me>
2022-12-08 21:12:10 -0800

    Added trade fees and open pnl calculations

---
+++ src/trades/fees.ts
@@ -0,0 +1,98 @@
+import {
+  Fee,
+  OpenInterest,
+  PairFundingFees,
+  PairParams,
+  PairRolloverFees,
+} from "./types";
+
+export const getClosingFee = (
+  posDai: number,
+  leverage: number,
+  pairIndex: number,
+  pairFee: Fee | undefined
+) => {
+  if (
+    posDai === undefined ||
+    leverage === undefined ||
+    pairIndex === undefined ||
+    pairFee === undefined
+  ) {
+    return 0;
+  }
+
+  const { closeFeeP, nftLimitOrderFeeP } = pairFee;
+
+  return (closeFeeP + nftLimitOrderFeeP) * posDai * leverage;
+};
+
+export type GetFundingFeeContext = {
+  currentBlock?: number;
+  pairParams?: PairParams;
+  pairFundingFees?: PairFundingFees;
+  openInterest?: OpenInterest;
+};
+
+export const getFundingFee = (
+  leveragedPosDai: number,
+  initialAccFundingFees: number,
+  buy: boolean,
+  openedAfterUpdate: boolean,
+  context: GetFundingFeeContext
+) => {
+  const { pairParams, pairFundingFees, openInterest, currentBlock } = context;
+
+  if (
+    !currentBlock ||
+    !openedAfterUpdate ||
+    pairParams === undefined ||
+    pairFundingFees === undefined ||
+    openInterest === undefined
+  )
+    return 0;
+
+  const { accPerOiLong, accPerOiShort, lastUpdateBlock } = pairFundingFees;
+  const { fundingFeePerBlockP } = pairParams;
+
+  const { long: longOi, short: shortOi } = openInterest;
+
+  const fundingFeesPaidByLongs =
+    (longOi - shortOi) * fundingFeePerBlockP * (currentBlock - lastUpdateBlock);
+
+  const pendingAccFundingFees = buy
+    ? accPerOiLong + fundingFeesPaidByLongs / longOi
+    : accPerOiShort + (fundingFeesPaidByLongs * -1) / shortOi;
+
+  return leveragedPosDai * (pendingAccFundingFees - initialAccFundingFees);
+};
+
+export type GetRolloverFeeContext = {
+  currentBlock?: number;
+  pairParams?: PairParams;
+  pairRolloverFees?: PairRolloverFees;
+};
+
+export const getRolloverFee = (
+  posDai: number,
+  initialAccRolloverFees: number,
+  openedAfterUpdate: boolean,
+  context: GetRolloverFeeContext
+) => {
+  const { pairParams, pairRolloverFees, currentBlock } = context;
+
+  if (
+    !currentBlock ||
+    !openedAfterUpdate ||
+    pairParams === undefined ||
+    pairRolloverFees === undefined
+  )
+    return 0;
+
+  const { accPerCollateral, lastUpdateBlock } = pairRolloverFees;
+  const { rolloverFeePerBlockP } = pairParams;
+
+  const pendingAccRolloverFees =
+    accPerCollateral + (currentBlock - lastUpdateBlock) * rolloverFeePerBlockP;
+
+  return posDai * (pendingAccRolloverFees - initialAccRolloverFees);
+};
---
+++ src/trades/pnl.ts
@@ -0,0 +1,88 @@
+import {
+  getClosingFee,
+  getFundingFee,
+  GetFundingFeeContext,
+  getRolloverFee,
+  GetRolloverFeeContext,
+} from "./fees";
+import { Fee, Pair, Trade, TradeInfo, TradeInitialAccFees } from "./types";
+
+export type GetPnlContext = {
+  fee: Fee | undefined;
+  pair: Pair | undefined;
+  maxGainP: number | undefined;
+} & GetRolloverFeeContext &
+  GetFundingFeeContext;
+
+export const getPnl = (
+  price: number | undefined,
+  trade: Trade,
+  tradeInfo: TradeInfo,
+  initialAccFees: TradeInitialAccFees,
+  useFees: boolean,
+  context: GetPnlContext
+) => {
+  if (!price) {
+    return;
+  }
+  const posDai = trade.initialPosToken * tradeInfo.tokenPriceDai;
+  const { openPrice, leverage } = trade;
+  const {
+    maxGainP,
+    currentBlock,
+    pairParams,
+    pairRolloverFees,
+    pairFundingFees,
+    openInterest,
+    fee,
+    pair,
+  } = context;
+  const maxGain = maxGainP === undefined ? Infinity : (maxGainP / 100) * posDai;
+
+  let pnlDai = trade.buy
+    ? ((price - openPrice) / openPrice) * leverage * posDai
+    : ((openPrice - price) / openPrice) * leverage * posDai;
+
+  pnlDai = pnlDai > maxGain ? maxGain : pnlDai;
+
+  if (useFees) {
+    pnlDai -= getRolloverFee(
+      posDai,
+      initialAccFees.rollover,
+      initialAccFees.openedAfterUpdate,
+      {
+        currentBlock,
+        pairParams,
+        pairRolloverFees,
+      }
+    );
+    pnlDai -= getFundingFee(
+      posDai * trade.leverage,
+      initialAccFees.funding,
+      trade.buy,
+      initialAccFees.openedAfterUpdate,
+      {
+        currentBlock,
+        pairParams,
+        pairFundingFees,
+        openInterest,
+      }
+    );
+  }
+
+  let pnlPercentage = (pnlDai / posDai) * 100;
+
+  // Can be liquidated
+  if (pnlPercentage <= -90) {
+    pnlPercentage = -100;
+  } else {
+    pnlDai -= getClosingFee(posDai, trade.leverage, trade.pairIndex, fee);
+    pnlPercentage = (pnlDai / posDai) * 100;
+  }
+
+  pnlPercentage = pnlPercentage < -100 ? -100 : pnlPercentage;
+
+  pnlDai = (posDai * pnlPercentage) / 100;
+
+  return [pnlDai, pnlPercentage];
+};
