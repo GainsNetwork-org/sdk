0648c489dad06023f6d395fc6516599aa12b8a5b
GainsNetwork_org <contact@gains.trade>
2023-01-01 21:38:29 +0100

    add isForexLowLiquidity and bump to 0.0.8

--- src/markets/forex.ts
+++ src/markets/forex.ts
@@ -29,3 +29,15 @@ export const isForexOpen = (dateToCheck: Date): boolean => {
 
   return !isClosed;
 };
+
+export const isForexLowLiquidity = (timestampToCheck: number) => {
+  const now = DateTime.fromMillis(timestampToCheck).setZone(
+    FOREX_MARKETS_TIME_ZONE_IANA
+  );
+  const hour = now.hour;
+  const minute = now.minute;
+  const isInDST = now.isInDST;
+
+  return (isInDST && (hour == 15 && minute >= 45) || (hour >= 16 && hour < 18)) ||
+    (!isInDST && (hour == 16 && minute >= 45) || (hour >= 17 && hour < 19));
+};
