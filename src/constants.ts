538f229a66a3d43e9011abe2d6924721df247835
0xCrumbs <97379465+0xcrumb@users.noreply.github.com>
2024-07-24 12:07:23 +0100

    update spread calcs for v9.2

--- src/constants.ts
+++ src/constants.ts
@@ -307,4 +307,4 @@ export const delistedPairIxs = new Set([
 
 export const delistedGroupsIxs = new Set([6, 7]);
 
-export const PROTECTION_CLOSE_FACTOR_BLOCKS = 1615; // ~1615 blocks per 7 minutes on Arbitrum
+export const DEFAULT_PROTECTION_CLOSE_FACTOR = 1;
