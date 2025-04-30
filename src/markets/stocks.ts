011483c0e917dffb457e3d33923e2b782831cb69
apaul243 <agarwalpulkit224@gmail.com>
2025-04-30 10:20:47 +0000

    update US stock market holidays

--- src/markets/stocks.ts
+++ src/markets/stocks.ts
@@ -16,28 +16,32 @@ export const isStocksOpen = (dateToCheck: Date): boolean => {
   const minute = now.minute;
 
   const isClosed =
-    // New Year's  2023
-    (month === 1 && dayOfMonth === 2) ||
-    // Martin Luther King, Jr. Day 2023
-    (month === 1 && dayOfMonth === 16) ||
-    // Washington's Birthday 2023
-    (month === 2 && dayOfMonth === 20) ||
-    // Good Friday 2023
-    (month === 4 && dayOfMonth === 7) ||
-    // Memorial Day 2023
-    (month === 5 && dayOfMonth === 29) ||
-    // Juneteenth National Independence Day 2023
+    // New Year's Day 2026
+    (month === 1 && dayOfMonth === 1) ||
+    // Martin Luther King, Jr. Day 2026
+    (month === 1 && dayOfMonth === 19) ||
+    // Presidents' Day 2026
+    (month === 2 && dayOfMonth === 16) ||
+    // Good Friday 2026
+    (month === 4 && dayOfMonth === 3) ||
+    // Memorial Day 2025
+    (month === 5 && dayOfMonth === 26) ||
+    // Juneteenth National Independence Day 2025
     (month === 6 && dayOfMonth === 19) ||
-    // Independence Day 2023
+    // Independence Day 2025
     (month === 7 && dayOfMonth === 4) ||
-    // Labor Day 2023
-    (month === 9 && dayOfMonth === 4) ||
-    // Thanksgiving Day 2022
-    (month === 11 && dayOfMonth === 23) ||
-    // Friday after Thanksgiving Day 2023 (closes early at 1PM)
-    (month === 11 && dayOfMonth === 24 && hour >= 13) ||
-    // Christmas Day 2023
+    // Labor Day 2025
+    (month === 9 && dayOfMonth === 1) ||
+    // Thanksgiving Day 2025
+    (month === 11 && dayOfMonth === 27) ||
+    // Black Friday 2025 (closes early at 1PM)
+    (month === 11 && dayOfMonth === 28 && hour >= 13) ||
+    // Christmas Eve 2025 (closes early at 1PM)
+    (month === 12 && dayOfMonth === 24 && hour >= 13) ||
+    // Christmas Day 2025
     (month === 12 && dayOfMonth === 25) ||
+    // Day before Independence Day 2025 (closes early at 1PM)
+    (month === 7 && dayOfMonth === 3 && hour >= 13) ||
     // Saturday
     weekday === 6 ||
     // Sunday
