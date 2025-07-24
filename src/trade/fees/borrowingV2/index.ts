6c6a3d513154d2e6be8bc4ae107473b29e01c12d
kukodev <kukodev@proton.me>
2025-07-24 14:08:51 +0200

    expose borrowing v2 builder exports

--- src/trade/fees/borrowingV2/index.ts
+++ src/trade/fees/borrowingV2/index.ts
@@ -128,4 +128,5 @@ export const borrowingFeeV2Utils = {
 export * as BorrowingFeeV2 from "./types";
 export { GetPairBorrowingFeeV2Context } from "./types";
 export * from "./converter";
+export * from "./builder";
 export * from "./fetcher";
